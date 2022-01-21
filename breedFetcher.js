66f986adb2a58de5ea4ec33c4205a4047e82916c
Khadija Mohamed <khadijam@live.ca>
2022-01-20 21:20:56 -0500

    fetching data for cat breed

---
+++ breedFetcher.js
@@ -0,0 +1,21 @@
+const request = require("request");
+const breeds = process.argv.slice(2);
+const breed = breeds[0];
+
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  
+const data = JSON.parse(body);
+
+  if (data.length === 0) {
+    console.log('The requested breed was not found.');
+  }
+  console.log(typeof body);
+  console.log(data);
+  console.log(typeof data);
+
+});
+
